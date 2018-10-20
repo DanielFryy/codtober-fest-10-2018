a41170b82698eb332b16985667c5d6e24fa3ef7c
DanielFryy <dgz1606@gmail.com>
2018-10-20 11:10:12 -0500

    First component

---
+++ pizza/src/app/components/place-your-order/place-your-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-your-order',
+  templateUrl: './place-your-order.component.html',
+  styleUrls: ['./place-your-order.component.css']
+})
+export class PlaceYourOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
